f6781a8547ceb69c266c0e543069813b274e9ca8
Himanshu Patel <patelhimanshu609@gmail.com>
2026-04-17 15:07:06 +0530

    [Himanshu Patel] refactor updating existing files

--- qma-service-registry/src/main/java/com/app/quantityservice/dto/TwoQuantityRequest.java
+++
@@ -1,9 +0,0 @@
-package com.app.quantityservice.dto;
-
-public class TwoQuantityRequest {
-
-    private QuantityDTO q1;
-    private QuantityDTO q2;
-
-    // getters and setters
-}
--- qma-service-registry/src/main/java/com/app/quantityservice/repository/IQuantityMeasurementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.quantityservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.quantityservice.entity.QuantityMeasurementEntity;
-
-public interface IQuantityMeasurementRepository
-        extends JpaRepository<QuantityMeasurementEntity, Long> {
-
-}
